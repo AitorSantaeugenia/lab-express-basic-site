989f3e307bc00269b07d14239a8d64fe69ace7fd
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-06-20 19:57:37 +0200

    changed few things

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 // Require Express
-const express = require('express');
+const express = require("express");
 
 // Express server handling requests and responses
 const app = express();
 
 // Make everything inside of /public available
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Home in /route:
-app.get('/', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+app.get("/", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home.html")
+);
 
 // About route:
-app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+app.get("/about", (request, response, next) =>
+  response.sendFile(__dirname + "/views/about.html")
+);
 
 // Works route:
-app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+app.get("/works", (request, response, next) =>
+  response.sendFile(__dirname + "/views/works.html")
+);
 
 // Works route:
-app.get('/gallery', (request, response, next) => response.sendFile(__dirname + '/views/gallery.html'));
+app.get("/gallery", (request, response, next) =>
+  response.sendFile(__dirname + "/views/gallery.html")
+);
 
 // Server Started
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
+app.listen(3000, () => console.log("My first app listening on port 3000!"));
